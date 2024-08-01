e37d8f3d2838f0ed8a887634c1d608315e58f519
Eric Z. Ayers <ericzundel@gmail.com>
2024-08-01 14:24:00 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# One Button Remote for SONOS bar mute
+
+This project uses CircuitPython on a microcontroller to 
+implement a one-button remote to mute/unmute the Sonos bar.
+
+
+
---
+++ code.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2024 Eric Z. Ayers
+#
+# SPDX-License-Identifier: Creative Commons Zero 1.0
+
+"""Collect training data from a color sensor and publish it to Adafruit IO """
+
+import board
+import busio
+import ipaddress
+import os
+import socketpool
+import time
+import wifi
+
+
+##################
+# *EDIT*
+
+#
+# End of editable config values
+##################
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print(
+        "WiFi secrets are kept in secrets.py, please add them there and don't commit them to git!"
+    )
+    raise
+
+def connect_to_wifi():
+    print()
+    print("Connecting to WiFi...", end="")
+
+    wifi.radio.connect(secrets["wifi_ssid"], secrets["wifi_password"])
+
+    #  prints IP address to REPL
+    print("Connected: IP address %s", wifi.radio.ipv4_address, end="")
+
+    #  pings Google DNS server to test connectivity
+    ipv4 = ipaddress.ip_address("8.8.4.4")
+    print(
+        " Internet up. Ping to google.com in: %f ms"
+        % (wifi.radio.ping(ipv4) * 1000)
+    )
+
+connect_to_wifi()
+
+while True:
+
+    time.sleep(1)

06225edb3268379f5f617e29979d414fe1567084
Eric Z. Ayers <ericzundel@gmail.com>
2024-08-01 14:25:44 -0400

    remove secrets.py

--- secrets.py
+++
@@ -1,7 +0,0 @@
-secrets = {
-    'wifi_ssid': 'School Network',  # WiFi network name to connect to
-    'wifi_password': 'password',    # Password to your network (if any)
-    'aio_username' : 'hwhacker',    # Your AdafruitIO username
-    'aio_key' : '0U812DEADBEEF',    # AdafruitIO API key. See the 'key' icon on the AdafruitIO home page
-    'aio-colorsensor-feed-id' : 'colorsensor-training-data', # The name of your feed in Adafruit IO
-}
